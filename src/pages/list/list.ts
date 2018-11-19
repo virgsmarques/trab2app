d444a400b51808d9bda487628336236285849340
Virgínia Marques <vmarques13@hotmail.com>
2018-11-18 19:39:11 -0500

    retirada de footer + retirada de exclusão

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -69,31 +69,12 @@ export class ListPage {
   }
 
 
-  showConfirm(animal){
-    const confirm = this.alertCtrl.create({
-      title: 'Confirmar arquivamento',
-      message: 'Você deseja arquivar este registro? Lembrando que uma vez arquivado não se pode mais restaurar!',
-      buttons: [
-        {
-          text: 'Discordo',
-          handler: () => {
-            console.log('Discordo escolhido');
-          }
-        },
-        {
-          text: 'Concordo',
-          handler: () => {
-            //  função para deletar registro 
-            this.firebaseService.delete(animal).then(d => {
-              this.toastrService.show('Registro excluído com sucesso!', 3000).present();
-                this.navCtrl.setRoot(ListPage);
-            })
-          }
-        }
-      ]
-    });
-    confirm.present();
-  }
+  // delete(animal){
+  //   this.firebaseService.delete(animal).then(d => {
+  //     this.toastrService.show('Registro excluído com sucesso!', 3000).present();
+  //       this.navCtrl.setRoot(ListPage);
+  //   })
+  // }
 
   editar(animal){
       this.navCtrl.push(EditPage,{
