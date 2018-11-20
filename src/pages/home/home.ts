dbe506a296cc6f49ccfecd0161678c5408be8171
Virgínia Marques <vmarques13@hotmail.com>
2018-11-19 22:53:50 -0500

    contagem de pedidos (total) incluida

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import firebase from 'firebase';
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
+  public animais = [];
+  public qtdTotal : number; // numero total de registros 
+
+  public animalList: Array<any>; // Is to store the list of animals we’re pulling from Firebase.
+  public loadeadAnimalList: Array<any>; 
+  public animalRef:firebase.database.Reference; // criação de referencia para puxar do firebase
+
   constructor(public navCtrl: NavController) {
+
+    this.animalRef = firebase.database().ref('/animais');
+
+        this.animalRef.on('value', animalList => { 
+          let animais = []; 
+          animalList.forEach(animal => {
+            animais.push({key: animal.key, ...animal.val()});
+            return false;
+          }); 
+
+
+          this.animalList = animais; 
+          this.loadeadAnimalList = animais;
+          this.qtdTotal = animalList.numChildren();
+          console.log(animalList.numChildren());
+      });
 
   }
 
